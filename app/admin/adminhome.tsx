3f14adfdcc4a628d41daac61a56da3522573dd14
Nipuna Fernando <nipunaf18@gmail.com>
2025-02-22 02:10:35 +0530

    add css to admin

--- app/admin/adminhome.tsx
+++ app/admin/adminhome.tsx
@@ -1,10 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import adminstyles from "./adminstyle";
+import styles from '../styles';
 
 const AdminHome = () => {
   return (
-    <View>
-      <Text>AdminHome</Text>
+    <View style={{flex:1}}>
+      <View style={adminstyles.button_view_container}>
+        <View style={adminstyles.button_view}>
+            <Text>Add a Song</Text>
+        </View>
+        <View style={adminstyles.button_view}>
+            <Text>Schedule Practices</Text>
+        </View>
+      </View>
     </View>
   )
 }
---
+++ app/admin/adminstyle.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const adminstyles = StyleSheet.create({
+  button_view_container: {
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+
+  button_view: {
+    flex:1,
+    width :"100%",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 10,
+    borderBlockColor: "black",
+    borderWidth: 2,
+    borderRadius: 15,
+    backgroundColor: "gray",
+  }
+});
+
+export default adminstyles;
